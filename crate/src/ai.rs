a5ae5722977000b3e432a6a3a50a2ca3697d245d
sepiropht <sergembotta@mailoo.org>
2019-01-14 16:32:59 +0100

    init

---
+++ crate/src/ai.rs
@@ -0,0 +1,25 @@
+use board::Board;
+
+pub struct Ai {
+    num_trial: u32
+}
+
+
+impl Ai {
+    fn trial(player: u32) {
+        unimplemented!();
+    }
+    fn update_scores(player: u32) {
+       unimplemented!();
+    }
+
+    fn get_best_move() {
+        unimplemented!();
+    }
+    fn clone_board(original: Board) {
+       unimplemented!();
+    }
+    fn player_move() {
+       unimplemented!();
+    }
+}
---
+++ crate/src/board.rs
@@ -0,0 +1,36 @@
+pub struct Board {
+    board: Vec<u32>
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Self {
+            board: vec![]
+        }
+    }
+    pub fn get_dim(&self) -> usize {
+        unimplemented!()
+    }
+
+    fn get_cell() {
+        unimplemented!();
+    }
+
+    fn player_move() {
+        unimplemented!();
+    }
+    
+    fn get_empty_cells() {
+        unimplemented!();
+    }
+
+    pub fn all_equal() {
+        unimplemented!();
+    }
+    
+    pub fn check_win() {
+        unimplemented!();
+    }
+    
+
+}
