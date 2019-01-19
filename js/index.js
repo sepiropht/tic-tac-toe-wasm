3aef5466cd6b3715f097539212609237323bf559
sepiropht <sergembotta@mailoo.org>
2019-01-20 00:28:54 +0100

    add react

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import("../crate/pkg").then(module => {
-  module.run();
+  ReactDOM.render(
+      <div>Hello Wasm!</div>,
+      document.getElementById('app')
+  )
+  debugger;
+  const board = module.Board.new(3);
+  const ai = module.Ai;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,18 @@ module.exports = {
     path: dist,
     filename: "bundle.js"
   },
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        }
+    ]
+  },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+  },
   devServer: {
     contentBase: dist,
   },
