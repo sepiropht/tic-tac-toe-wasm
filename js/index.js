f102b52e149569794c1173f0a66734494ddc4f06
sepiropht <sergembotta@mailoo.org>
2019-02-05 01:16:16 +0100

    react/hooks version works!

--- js/index.js
+++ js/index.js
@@ -2,222 +2,218 @@ import React from "react";
 import ReactDOM from "react-dom";
 import BoardJS from "./board";
 import aiJS from "./ai";
-import { useState } from "react";
+import { useState, useReducer, useEffect } from "react";
+
+let modelBoard, ai;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "updateBoard":
+      const { x, y, player } = action.payload;
+      return {
+        ...state,
+        board: state.board.map((rows, rowind) =>
+          rows.map((cell, cellind) =>
+            x === rowind && y === cellind ? player : cell
+          )
+        )
+      };
+    case "toogleFreezeBoard":
+      return {
+        ...state,
+        freezeBoard: action.payload
+      };
+    case "setWinner":
+      return {
+        ...state,
+        winner: action.payload
+      };
+    /*case "nextPlayer":
+      return {
+        ...state,
+        player: state.player === 1 ? 2 : 1
+      };
+    */
+    case "initboard":
+      return {
+        ...state,
+        board: action.payload
+      };
+    case "init": {
+      return {
+        ...state,
+        board: action.payload,
+        winner: 0,
+        freezeBoard: false
+      };
+    }
+  }
+  return state;
+}
 
 import("../crate/pkg").then(({ Board, Ai }) => {
-  let ai;
-  class TicTacToe extends React.Component {
-    constructor(props) {
-      super(props);
-      let board = [];
-      this.board = new Board(props.width);
-      for (let i = 0; i < props.width; i++) {
+  function TicTacToe({ isWasm, width, singlePlayer, reset }) {
+    useEffect(() => {
+      const board = [];
+      for (let i = 0; i < width; i++) {
         let row = [];
-        for (let j = 0; j < props.width; j++) {
+        for (let j = 0; j < width; j++) {
           row.push(0);
         }
         board.push(row);
       }
-      ai = props.isWasm ? new Ai() : aiJS;
-      this.state = {
-        player: 1,
-        freezeBoard: false,
-        winner: false,
-        board,
-        isWasm: props.isWasm
-      };
-    }
+      dispatch({ type: "init", payload: board });
+      aiInit();
+    }, [width, isWasm, ai]);
 
-    nextPlayer() {
-      return this.state.player === 1 ? 2 : 1;
-    }
+    const initialState = {
+      isWasm,
+      player: 1
+    };
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [player, setPlayer] = useState(1);
 
     // Place a move on the board and check for a winner.
-    move(x, y, player, callback) {
-      this.board.playerMove(x, y, player);
-      const winner = this.board.checkWin();
+    function move(x, y, player, callback) {
+      modelBoard.playerMove(x, y, player);
+
+      let winner = modelBoard.checkWin();
       if (isNaN(winner)) {
         winner = winner === 0;
       }
       if (winner) {
-        this.setState({ winner, freezeBoard: true });
+        dispatch({ type: "setWinner", payload: winner });
+        dispatch({ type: "toogleFreezeBoard", payload: true });
       } else {
         callback();
       }
     }
 
     // Handle a player's move, and switch to the next player.
-    playerMove(event) {
+    function playerMove(event) {
       let [x, y] = event.target.dataset.cell.split("_");
       x = parseInt(x);
       y = parseInt(y);
-      //console.log('getCell', this.board.getCell(x, y))
-      const cellEmpty = this.board.getCell(x, y) === 0;
+
+      const cellEmpty = modelBoard.getCell(x, y) === 0;
       if (cellEmpty) {
-        this.move(x, y, this.state.player, () => {
-          this.state.board[x][y] = this.state.player;
-          if (this.props.singlePlayer) {
-            this.setState(
-              { player: this.nextPlayer(), freezeBoard: true },
-              this.aiMove
-            );
+        move(x, y, player, () => {
+          dispatch({
+            type: "updateBoard",
+            payload: { x, y, player }
+          });
+          if (singlePlayer) {
+            setPlayer(player === 1 ? 2 : 1);
+            dispatch({ type: "toogleFreezeBoard", payload: true });
+            aiMove(player === 1 ? 2 : 1);
           } else {
-            this.setState({ player: this.nextPlayer() });
+            setPlayer(player === 1 ? 2 : 1);
+            //dispatch({ type: "nextPlayer"});
           }
         });
       }
     }
 
-    // Make an AI move, with a small delay for a more natural response time.
-    aiMove() {
-      //console.log("BBBBBBBBoard", board);
-      const point = ai.aiMove(this.board, this.state.player);
+    function aiMove(player = 1) {
+      const point = ai.aiMove(modelBoard, player);
       let x, y;
-      if (this.props.isWasm) {
+      if (isWasm) {
         x = point.getX();
         y = point.getY();
       } else {
         [x, y] = point;
       }
-      this.state.board = this.state.board.map((rows, rowind) =>
-        rows.map((cell, cellind) =>
-          x === rowind && y === cellind ? this.state.player : cell
-        )
-      );
-      this.setState(({ board }) => ({
-        board: board.map((rows, rowind) =>
-          rows.map((cell, cellind) =>
-            x === rowind && y === cellind ? this.state.player : cell
-          )
-        )
-      }));
-      //this.state.board[x][y] = this.state.player;
-      this.move(x, y, this.state.player, () => {
-        this.setState({ player: this.nextPlayer(), freezeBoard: false });
+      dispatch({
+        type: "updateBoard",
+        payload: { x, y, player }
+      });
+
+      move(x, y, player, () => {
+        setPlayer(player === 1 ? 2 : 1);
+        dispatch({ type: "toogleFreezeBoard", payload: false });
       });
     }
 
     // Determine which player will be the AI in single player mode,
     // and make the first move if appropriate.
-    aiInit() {
-      if (this.props.singlePlayer) {
+    function aiInit() {
+      if (singlePlayer) {
         const aiPlayer = Math.floor(Math.random() * 2) + 1;
         if (aiPlayer === 1) {
-          this.aiMove();
+          aiMove(1);
         }
       }
     }
 
-    reset() {
-      this.board = this.props.isWasm
-        ? new Board(this.props.width)
-        : new BoardJS(this.props.width);
-      const board = [];
 
-      for (let i = 0; i < this.props.width; i++) {
-        let row = [];
-        for (let j = 0; j < this.props.width; j++) {
-          row.push(0);
+    let boardView = isWasm ? state.board || [] : modelBoard.board || [];
+    let announcement;
+
+    if (state.winner) {
+      const msg =
+        state.winner > 2 ? "It's a tie!" : `Player ${state.winner} wins!`;
+      announcement = (
+        <div className="announcement">
+          <p>{msg}</p>
+          <button onClick={reset}>Reset</button>
+        </div>
+      );
+    }
+    const grid = boardView.map((row, rowInd) => {
+      const cells = row.map((cell, cellInd) => {
+        const classString =
+          cell > 0 ? (cell === 1 ? "cell-p1" : "cell-p2") : "cell";
+        const coords = `${rowInd}_${cellInd}`;
+        let clickHandler;
+        if (!state.freezeBoard) {
+          clickHandler = playerMove;
         }
-        board.push(row);
-      }
-      this.state.board = board;
-      this.setState(({ player, freezeBoard, winner, board }) => ({
-        player: 1,
-        freezeBoard: false,
-        winner: false,
-        board
-      }));
-      this.aiInit();
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-      if (this.props.isWasm !== prevProps.isWasm) {
-        ai = this.props.isWasm ? new Ai() : aiJS;
-        this.reset();
-      }
-    }
-
-    componentDidMount() {
-      this.aiInit();
-    }
-
-    render() {
-      let board;
-      if (this.props.isWasm) {
-        board = this.state.board;
-      } else {
-        board = this.board.board || [];
-      }
-      let announcement;
-
-      if (this.state.winner) {
-        const msg =
-          this.state.winner > 2
-            ? "It's a tie!"
-            : `Player ${this.state.winner} wins!`;
-        announcement = (
-          <div className="announcement">
-            <p>{msg}</p>
-            <button onClick={this.reset.bind(this)}>Reset</button>
-          </div>
-        );
-      }
-      //console.log({board})
-      const grid = board.map((row, rowInd) => {
-        const cells = row.map((cell, cellInd) => {
-          const classString =
-            cell > 0 ? (cell === 1 ? "cell-p1" : "cell-p2") : "cell";
-          const coords = `${rowInd}_${cellInd}`;
-          let clickHandler;
-
-          if (!this.state.freezeBoard) {
-            clickHandler = this.playerMove.bind(this);
-          }
-
-          return (
-            <div
-              className={classString}
-              key={cellInd}
-              onClick={clickHandler}
-              data-cell={coords}
-            />
-          );
-        });
 
         return (
-          <div className="row" key={rowInd}>
-            {cells}
-          </div>
+          <div
+            className={classString}
+            key={cellInd}
+            onClick={clickHandler}
+            data-cell={coords}
+          />
         );
       });
 
       return (
-        <div className="grid">
-          {grid}
-          {announcement}
+        <div className="row" key={rowInd}>
+          {cells}
         </div>
       );
-    }
+    });
+
+    return (
+      <div className="grid">
+        {grid}
+        {announcement}
+      </div>
+    );
   }
 
   function App() {
     // Set Wasm by default
     const [selectedOption, setOption] = useState("wasm");
     const handleChange = e => setOption(e.target.value);
-
-    // 3*3 grid by default
     const [width, setWidth] = useState(3);
+    ai = selectedOption === "wasm" ? new Ai() : aiJS;
+    modelBoard =
+      selectedOption === "wasm" ? new Board(width) : new BoardJS(width);
+
     const handleChangeWidth = e =>
       e.target.value > 2 && e.target.value < 11
         ? setWidth(parseInt(e.target.value))
         : width;
-    const Game = (
-      <TicTacToe
-        width={width}
-        isWasm={selectedOption === "wasm"}
-        singlePlayer={true}
-      />
-    );
+
+    function reset() {
+      ai = undefined;
+      ai = selectedOption === "wasm" ? new Ai() : aiJS;
+      modelBoard =
+        selectedOption === "wasm" ? new Board(width) : new BoardJS(width);
+    }
     return (
       <>
         <h1> TicTacToe in wasm </h1>
@@ -258,7 +254,12 @@ import("../crate/pkg").then(({ Board, Ai }) => {
             </label>
           </div>
         </form>
-        {Game}
+        <TicTacToe
+          width={width}
+          isWasm={selectedOption === "wasm"}
+          reset={reset}
+          singlePlayer={true}
+        />{" "}
       </>
     );
   }
